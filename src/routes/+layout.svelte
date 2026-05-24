e7f907d4abc65d0bb71d592296b1ad3c9c7c25eb
Xiyuan Guo <tommyguo039@gmail.com>
2026-05-24 16:54:01 -0400

    fix(web): remove mounted check to enable SSR and fix blank screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,48 +27,45 @@
 
 <div class="mx-auto min-h-screen max-w-screen-2xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
 	<div class="lg:flex lg:justify-between lg:gap-4">
-		{#if mounted}
-			<header
-				in:fly={{ x: -20, y: -20, duration: 1000, delay: 200 }}
-				class="lg:sticky lg:top-0 lg:flex lg:h-screen lg:w-4/12 lg:flex-col lg:justify-between lg:py-24"
-			>
-				<div>
-					<h1 class="text-cloud text-4xl font-bold tracking-tight sm:text-5xl">
-						<a href="/">Xiyuan (Tommy) Guo</a>
-					</h1>
-					<h2 class="text-cloud-dim mt-3 text-lg font-medium tracking-tight sm:text-xl">
-						Systems | Software Engineer
-					</h2>
-					<p class="text-cloud-dim mt-4 max-w-xs leading-normal">
-						Architecting high-performance systems and low-latency terminal architecture
-						for power users.
-					</p>
+		<header
+			class="lg:sticky lg:top-0 lg:flex lg:h-screen lg:w-4/12 lg:flex-col lg:justify-between lg:py-24"
+		>
+			<div>
+				<h1 class="text-cloud text-4xl font-bold tracking-tight sm:text-5xl">
+					<a href="/">Xiyuan (Tommy) Guo</a>
+				</h1>
+				<h2 class="text-cloud-dim mt-3 text-lg font-medium tracking-tight sm:text-xl">
+					Systems | Software Engineer
+				</h2>
+				<p class="text-cloud-dim mt-4 max-w-xs leading-normal">
+					Architecting high-performance systems and low-latency terminal architecture
+					for power users.
+				</p>
 
-					<nav class="mt-16 hidden lg:block">
-						<ul class="w-max">
-							{#each ['about', 'experience', 'projects'] as item}
-								<li>
-									<a
-										href="#{item}"
-										class="group text-cloud-dim hover:text-cloud flex items-center py-3 text-xs font-bold tracking-widest uppercase"
-									>
-										<span
-											class="bg-cloud-dim group-hover:bg-reze mr-4 h-px w-8 transition-all duration-300 group-hover:w-16"
-										></span>
-										{item}
-									</a>
-								</li>
-							{/each}
-						</ul>
-					</nav>
-				</div>
+				<nav class="mt-16 hidden lg:block">
+					<ul class="w-max">
+						{#each ['about', 'experience', 'projects'] as item}
+							<li>
+								<a
+									href="#{item}"
+									class="group text-cloud-dim hover:text-cloud flex items-center py-3 text-xs font-bold tracking-widest uppercase"
+								>
+									<span
+										class="bg-cloud-dim group-hover:bg-reze mr-4 h-px w-8 transition-all duration-300 group-hover:w-16"
+									></span>
+									{item}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</nav>
+			</div>
 
-				<Socials />
-			</header>
+			<Socials />
+		</header>
 
-			<main in:fly={{ y: 20, duration: 1000, delay: 600 }} class="pt-24 lg:w-7/12 lg:py-24">
-				<slot />
-			</main>
-		{/if}
+		<main class="pt-24 lg:w-7/12 lg:py-24">
+			<slot />
+		</main>
 	</div>
 </div>
